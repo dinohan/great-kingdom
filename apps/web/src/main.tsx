c5d2e5dc4ab0bca9df5ca22a4db85f86ce592410
dinohan <dinohan.dev@gmail.com>
2023-08-16 00:27:46 +0900

    feat(web/CreateGameForm): add create game mutation

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,13 @@
+import './index.css'
 import React from 'react'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import Games from './pages/Games'
 import GamesNew from './pages/Games/New'
 import Home from './pages/Home'
-import './index.css'
 
 const router = createBrowserRouter([
   {
@@ -23,8 +24,12 @@ const router = createBrowserRouter([
   },
 ])
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
