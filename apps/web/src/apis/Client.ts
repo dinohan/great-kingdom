e6a5282f79c864829dbc09393d5c009fa8e1472f
dinohan <dinohan.dev@gmail.com>
2023-08-16 03:52:18 +0900

    feat: axios 설치, API 통신에 사용하는 client 모듈 구현

---
+++ apps/web/src/apis/Client.ts
@@ -0,0 +1,32 @@
+import { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+import Fetcher from './Fetcher'
+
+class Client {
+  get<T = unknown, R extends AxiosResponse<T> = AxiosResponse<T>, D = unknown>(
+    path: string,
+    config?: AxiosRequestConfig
+  ) {
+    return new Fetcher<T, R, D>({ ...config, url: path, method: 'GET' })
+  }
+
+  post<T = unknown, R extends AxiosResponse<T> = AxiosResponse<T>, D = unknown>(
+    path: string,
+    data: D,
+    config?: AxiosRequestConfig
+  ) {
+    return new Fetcher<T, R, D>({ ...config, url: path, method: 'POST', data })
+  }
+
+  put<T = unknown, R extends AxiosResponse<T> = AxiosResponse<T>, D = unknown>(
+    path: string,
+    data: D,
+    config?: AxiosRequestConfig
+  ) {
+    return new Fetcher<T, R, D>({ ...config, url: path, method: 'PUT', data })
+  }
+}
+
+const client = new Client()
+
+export default client
