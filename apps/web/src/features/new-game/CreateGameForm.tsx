a73ce5e90fe3e26a9de5dca758eef83e4762f364
dinohan <dinohan.dev@gmail.com>
2023-08-16 00:27:46 +0900

    feat(web/CreateGameForm): add create game mutation

--- apps/web/src/features/new-game/CreateGameForm.tsx
+++ apps/web/src/features/new-game/CreateGameForm.tsx
@@ -1,14 +1,46 @@
+import { useRef } from 'react'
+
+import { useMutation } from '@tanstack/react-query'
+import { useNavigate } from 'react-router-dom'
+
 function CreateGameForm() {
+  const navigate = useNavigate()
+
+  const { mutate } = useMutation({
+    mutationFn: ({ title }: { title?: string }) => {
+      return new Promise<{
+        id: string
+      }>((resolve) => {
+        console.log(title)
+        resolve({ id: 'abcd' })
+      })
+    },
+    onSuccess: (data) => {
+      console.log(data)
+      false && navigate(`/games/${data.id}`)
+    },
+  })
+
+  const titleRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const title = titleRef.current?.value
+    mutate({ title })
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="title">
         <span>Game name</span>
       </label>
       <input
+        ref={titleRef}
         id="title"
         type="text"
         placeholder="Game name"
       />
+      <button type="submit">Create game</button>
     </form>
   )
 }
