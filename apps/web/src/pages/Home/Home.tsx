3ae6ad931af4412048e4b437db6ecdcfbf14bc47
dinohan <dinohan.dev@gmail.com>
2023-08-17 23:56:55 +0900

    fix(web/Home): add styles to home page

--- apps/web/src/pages/Home/Home.tsx
+++ apps/web/src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
 
+import styles from './Home.module.scss'
+
 function Home() {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Link to="/games/new">New Game</Link>
       <Link to="/login">Login</Link>
     </div>
