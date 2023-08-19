f38990ec7c86ffaac25444babcaec609001e1c36
dinohan <dinohan.dev@gmail.com>
2023-08-19 16:02:10 +0900

    feat(web): Game 페이지에 게임 정보 표시

--- apps/web/src/pages/Games/Game/Game.tsx
+++ apps/web/src/pages/Games/Game/Game.tsx
@@ -4,20 +4,34 @@ import Grid from '@/components/Grid'
 import { useGame } from '@/features/games'
 import useJoin from '@/features/games/useJoin'
 
+import styles from './Game.module.scss'
+
 function Game() {
-  const { data } = useGame()
+  const { data: game } = useGame()
 
   useJoin()
 
-  if (!data) {
+  if (!game) {
     return <div>Loading...</div>
   }
 
-  const board = build(getBoardFromLog(data.log))
+  const board = build(getBoardFromLog(game.log))
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Grid board={board} />
+      <section className={styles.meat}>
+        <h1>{game.title}</h1>
+        <div>
+          {game.players.black} vs {game.players.white}
+          {game.score.black} - {game.score.white}
+        </div>
+        <ol>
+          {game.log.map((land, index) => (
+            <li key={index}>{land}</li>
+          ))}
+        </ol>
+      </section>
     </div>
   )
 }
