293f93f41ce614bed859931b8acfbbb07f74788e
dinohan <dinohan.dev@gmail.com>
2023-08-20 05:51:27 +0900

    fix: 게임 버튼 스타일 수정

--- apps/web/src/features/games/components/GameButton.tsx
+++ apps/web/src/features/games/components/GameButton.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from 'react'
 
+import classNames from 'classnames'
+
 import useLand from '@/features/lands/useLand'
 import { useGameStore } from '@/store/game/useGameStore'
 
 import useGame from '../hooks/useGame'
+
+import styles from './GameButton.module.scss'
 
 function GameButton() {
   const { game, isUserTurn } = useGame()
@@ -28,21 +32,26 @@ function GameButton() {
   }
 
   return (
-    <>
+    <div className={styles.wrapper}>
       <button
-        disabled={!isUserTurn}
+        className={classNames(styles.submit, {
+          [styles.notReady]: !isUserTurn,
+        })}
+        disabled={!isUserTurn || !temporaryCoordinate}
         onClick={handleSubmit}
       >
-        착수
+        {isUserTurn ? '착수' : '상대 턴'}
       </button>
-      <button
-        disabled={!isUserTurn}
-        onClick={handlePass}
-      >
-        건너뛰기
-      </button>
-      <button>기권</button>
-    </>
+
+      <div className={styles.subActions}>
+        <button
+          disabled={!isUserTurn}
+          onClick={handlePass}
+        >
+          건너뛰기
+        </button>
+      </div>
+    </div>
   )
 }
 
