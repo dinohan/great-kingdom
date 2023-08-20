55a3a3a99d2a0621c4d4e072e3727e31a44036d1
dinohan <dinohan.dev@gmail.com>
2023-08-20 18:13:41 +0900

    fix(web/log): scroll to bottom on resize

--- apps/web/src/features/games/components/Log.tsx
+++ apps/web/src/features/games/components/Log.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Land } from 'models'
 
 import styles from './Log.module.scss'
 
 function Log({ log }: { log: Land[] }) {
+  const ref = React.useRef<HTMLDivElement>(null)
+
+  useEffect(function initializeScrollBottom() {
+    const target = ref.current
+
+    if (!target) {
+      return
+    }
+
+    const resizeObserver = new ResizeObserver(function scrollToBottom() {
+      target.scrollTop = target.scrollHeight
+    })
+
+    resizeObserver.observe(ref.current)
+
+    return function cleanup() {
+      resizeObserver.disconnect()
+    }
+  }, [])
+
   return (
-    <div className={styles.wrapper}>
+    <div
+      ref={ref}
+      className={styles.wrapper}
+    >
       <ol>
         {log.map((land, index) => (
           <React.Fragment key={index}>
